812aefe533d4a77e72b3cf049fcc933e8566a2ff
Besik Betchvaia <besik.betchvaia@outlook.com>
2020-12-22 23:10:57 +0100

    Add project files.

---
+++ CarRadioCompanion/Data/Radiosender.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarRadioCompanion
+{
+    public class Radiosender
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Url { get; set; }
+    }
+}
